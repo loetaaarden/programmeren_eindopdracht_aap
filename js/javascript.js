572a62e54bd4abf7b244a490723d7caa61ab0fff
loetaaarden <loetaaarden@gmail.com>
2024-06-01 17:48:19 +0200

    hajvjda

--- js/javascript.js
+++ js/javascript.js
@@ -10,8 +10,7 @@ const btnachtergrond = document.getElementById("veranderAchtergrond");
 const achtergrondPlaatje = document.querySelector("#achtergrond");
 
 //dit zijn de constante voor de muziek
-const btnmuziek = document.getElementById("btnMuziek");
-const btnStopMuziek = document.getElementById("btnStopMuziek");
+const btnMuziek = document.getElementById("btnMuziek");
 const audioHein = new Audio("audio/Littletricks().mp3"); // deze muziek is van hein, die wij mochten gebruiken
 
 //dit zijn de constante voor de aap die beweegt
@@ -22,15 +21,25 @@ const beweegAudio = new Audio("audio/beweeg_geluid.mp3");// deze audio heeft ing
 //dit zijn de constante voor de banaan
 const btnBanaan = document.getElementById("btnBanaan");
 const eetAudio = new Audio("audio/eet_geluid.mp3"); // deze audio heeft ingmar voor mij opgenomen en die mocht ik van hem gebruiken.
+const banaan =  document.querySelector("#banaan");
 
 //dit zijn de constante voor het water
 const btnWater = document.getElementById("btnWater");
 const waterAudio = new Audio("audio/water_geluid.mp3"); // deze audio heeft ingmar voor mij opgenomen en die mocht ik van hem gebruiken.
+const water =  document.querySelector("#water");
 
 //dit zijn de constante voor de percentages
 const voedselTekst = document.getElementById("voedselProcent");
 const energieTekst = document.getElementById("energieProcent");
 const plezierTekst = document.getElementById("plezierProcent");
+
+//dit zijn de constante voor de jumpscare
+const jumpscare = document.getElementById("jumpscare");
+// const jumpscareAudio = new Audio("audio/jumpscare_audio.mp3");
+
+//dit zijn de constante voor het gameoverscherm
+const gameover = document.querySelector("#gameover");
+const btnReset = document.getElementById("reset");
 
 //LET
 
@@ -39,17 +48,26 @@ let maxVoedsel = 100;
 let maxEnergie = 100;
 let maxPlezier = 100;
 
+//deze let is voor het checken of het game over is 
+let gameoverInterval = setInterval(GameOverScherm, 1000);
+
+//deze let is voor de muziek button
+let muziekStatus = false
+
 //FUNCTIES
 
 //speel muziek
 function SpeelMuziek() {
-    audioHein.play(); // deze tag heb ik voor het stukje eigen onderzoek gevonden op de site stackoverflow.com
-}
-
-//stop muziek
-function StopMuziek() {
-    audioHein.pause(); 
-    audiocurrentTime = 0; //deze tag heb ik voor het stukje eigen onderzoek op codetogo.io gevonden
+    if (muziekStatus == true){
+        btnMuziek.src = "images/muziek_aan.png"
+        audioHein.play(); // deze tag heb ik voor het stukje eigen onderzoek gevonden op de site stackoverflow.com
+        muziekStatus = false;
+    } else {
+        audioHein.pause(); 
+        audioHein.currentTime = 0; //deze tag heb ik voor het stukje eigen onderzoek op codetogo.io gevonden
+        muziekStatus = true;
+        btnMuziek.src = "images/muziek_uit.png"
+    }
 }
 
 //verander de naam
@@ -58,6 +76,7 @@ function VeranderNaam () {
     naam.textContent = "Hallo ik ben " + namen[getal] + " de aap";
 }
 
+//percentages verlagen van het voedsel
 function VoedselOmlaag() {
     if (maxVoedsel > 0) {
         maxVoedsel -= 1;
@@ -65,6 +84,7 @@ function VoedselOmlaag() {
     voedselTekst.textContent = maxVoedsel + "%";
 }
 
+//percentages verlagen van het voedsel
 function EnergieOmlaag() {
     if (maxEnergie > 0) {
         maxEnergie -= 1;
@@ -72,6 +92,7 @@ function EnergieOmlaag() {
     energieTekst.textContent = maxEnergie + "%";
 }
 
+//percentages verlagen van het voedsel
 function PlezierOmlaag() {
     if (maxPlezier > 0) {
         maxPlezier -= 1;
@@ -79,15 +100,23 @@ function PlezierOmlaag() {
     plezierTekst.textContent = maxPlezier + "%";
 }
 
+//percentages verhogen doordat je hem een banaan voert
 function VoerBanaan() {
     if (maxVoedsel < 95 && maxPlezier < 95) {
         maxVoedsel += 5;
         maxPlezier += 5;
+        banaan.classList.toggle("banaan");
+        eetAudio.play();
+        setTimeout(WegBanaan, 1600);
     }
-    eetAudio.play();
 }
 
-function VeranderAap () {
+function WegBanaan(){
+    banaan.classList.toggle("banaan");
+}
+
+//percentages verhogen doordat je hem laat bewegen
+function VeranderAap() {
     if (maxEnergie < 95 && maxPlezier < 95) {
         maxEnergie += 5;
         maxPlezier += 5;
@@ -103,14 +132,46 @@ function VeranderAap () {
     } 
 }  
 
-function VoerWater () {
+//percentages verhogen doordat je hem water voert
+function VoerWater() {
     if (maxVoedsel < 95 && maxEnergie < 95) {
         maxEnergie += 5;
         maxVoedsel += 5;
+        waterAudio.play()
+        water.classList.toggle("water");
+        waterAudio.play()
+        setTimeout(WegWater, 1600);
     }
-    waterAudio.play()
 }
 
+function WegWater() {
+    water.classList.toggle("water");
+}
+
+function ShowJumpscare() {
+    // jumpscareAudio.play();
+    jumpscare.classList.toggle("jumpscare"); //de jumpscare is een animatie     
+}
+
+function HideJumpscare() {
+    jumpscare.classList.toggle("jumpscare");
+    // jumpscareAudio.pause(); 
+    // jumpscareAudio.currentTime = 0;
+}
+
+function GameOverScherm() {
+    if(maxEnergie == 0 || maxPlezier == 0 || maxVoedsel == 0) {
+        aapje.src = "./images/dode_aap.png"; 
+        clearInterval(gameoverInterval);
+        gameover.classList.toggle("gameover");
+    } 
+}
+
+function PlayAgain() {
+    window.location.reload(); //source: https://sentry.io/answers/how-do-i-refresh-a-page-using-javascript/
+}
+
+//Verander de achtergrond
 function VeranderAchtergrond () {
     const getal = Math.floor(Math.random() * 3);
     if(getal == 0) {
@@ -122,14 +183,26 @@ function VeranderAchtergrond () {
     }  
 }
 
-btnmuziek.addEventListener('click', SpeelMuziek);
-btnStopMuziek.addEventListener("click", StopMuziek);
+//SETINTERVALS
+setInterval(VoedselOmlaag, 1400);
+setInterval(EnergieOmlaag, 1000);
+setInterval(PlezierOmlaag, 1800);
+
+// setTimeout(ShowJumpscare, 30000);
+// setTimeout(HideJumpscare, 31000);
+
+//EVENTLISTNERS
+
+//eventlistner voor muziek
+btnMuziek.addEventListener('click', SpeelMuziek);
+
+//eventlistner voor buttons links
 btnnaam.addEventListener("click", VeranderNaam);
-btnAap.addEventListener("click", VeranderAap);
 btnachtergrond.addEventListener("click", VeranderAchtergrond);
+
+//eventlistner voor buttons rechts
+btnAap.addEventListener("click", VeranderAap);
 btnBanaan.addEventListener("click", VoerBanaan);
 btnWater.addEventListener("click", VoerWater);
 
-setInterval(VoedselOmlaag, 1000);
-setInterval(EnergieOmlaag, 1000);
-setInterval(PlezierOmlaag, 1000);
+btnReset.addEventListener("click", PlayAgain);
